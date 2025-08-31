3bc9ee19913b79c9b64127ed0293777661f90829
PAUL.s <96791484+Gretxleb@users.noreply.github.com>
2025-08-31 16:47:29 +0500

    Create SimpleAccount.java

---
+++ SimpleAccount.java
@@ -0,0 +1,24 @@
+// SimpleAccount.java
+public class SimpleAccount extends Account {
+    public SimpleAccount(long initialBalance) {
+        super(initialBalance);
+    }
+
+    @Override
+    public boolean add(long amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean pay(long amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
