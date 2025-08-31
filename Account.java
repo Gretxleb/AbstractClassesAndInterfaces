3b9e21ed632a89dfcaef03e7409476b3a3330cd4
PAUL.s <96791484+Gretxleb@users.noreply.github.com>
2025-08-31 16:46:41 +0500

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,28 @@
+// Account.java
+public abstract class Account {
+    protected long balance;
+
+    public Account(long initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public abstract boolean add(long amount);
+    public abstract boolean pay(long amount);
+
+    public boolean transfer(Account account, long amount) {
+        if (pay(amount)) {
+            if (account.add(amount)) {
+                return true;
+            } else {
+                // Если пополнение не удалось, возвращаем деньги на исходный счет
+                add(amount);
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+}
